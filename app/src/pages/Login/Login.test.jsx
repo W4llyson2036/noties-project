e31dd0d2c2088414486de99b210b5b9be7cb483a
W4llyson2036 <wallysonivo0111@gmail.com>
2024-02-21 10:19:41 -0300

    test component Login

--- app/src/pages/Login/Login.test.jsx
+++ app/src/pages/Login/Login.test.jsx
@@ -1,18 +1,37 @@
 import { render, screen } from "@testing-library/react";
 import { Login } from "./Login";
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect } from "vitest";
+import { BrowserRouter } from "react-router-dom";
 
-//back here later, i didnt understand why this is not working
 describe('load Login', () => {
-    test.skip('title', () => {
-        render(<Login />)
-        const title = screen.getByText('noTies');
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+            <Login />
+        </BrowserRouter>
+        );
+    });
+
+    it('render title', () => {
+        const title = screen.getByText('noTies')
         expect(title).toBeInTheDocument();
     });
 
-    // test('button create account', () => {
-    //     render(<Login />)
-    //     const createNewAccount = screen.getByText('create new account');
-    //     expect(createNewAccount).toBeInTheDocument();
-    // })
-})
+    it('render inputs', () => {
+        const gmailInput = screen.getByPlaceholderText('Email');
+        const passwordInput = screen.getByPlaceholderText('Password');
+
+        expect(gmailInput).toBeInTheDocument();
+        expect(passwordInput).toBeInTheDocument();
+    });
+
+    it('render button create account', () => {
+        const createNewAccount = screen.getByText('Create new account');
+        expect(createNewAccount).toBeInTheDocument();
+    });
+
+    it('render text create new account', () => {
+        const createNewAccountBtn = screen.getByText('Create new account');
+        expect(createNewAccountBtn).toBeInTheDocument();
+    });
+});
