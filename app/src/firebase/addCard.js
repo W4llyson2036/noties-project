f260ba4401fd7fc066438d7ef5286ff61adb6090
W4llyson2036 <wallysonivo0111@gmail.com>
2024-02-28 09:50:57 -0300

    add date and time when creating a card

--- app/src/firebase/addCard.js
+++ app/src/firebase/addCard.js
@@ -1,8 +1,10 @@
 import { db } from "./firebaseConfig";
 import { auth } from "./firebaseConfig";
-import { collection, doc, addDoc, getDocs } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 export async function addCard(card, param) {
+    const date = new Date();
+
     try {
         const collRef = collection(db, `user - ${auth.currentUser.uid}`);
         const querySnapshot = await getDocs(collRef);
@@ -11,11 +13,25 @@ export async function addCard(card, param) {
         
         await addDoc(subCollectionRef, {
             cardFront: card.cardFront,
-            cardBack: card.cardBack
+            cardBack: card.cardBack,
+            createdAt: {
+                date: date.toDateString(),
+                hour: formateHours(),
+            },
         });
 
     } catch (error) {
         console.error('Error fetching documents: ', error);
         throw error;
-    }
-}
+    };
+};
+
+function formateHours() {
+    const TIME = new Date();
+    const HOUR = TIME.getHours() < 10 ? `0${TIME.getHours()}` : TIME.getHours();
+    const MINUTE = TIME.getMinutes() < 10 ? `0${TIME.getMinutes()}` : TIME.getMinutes();
+    const SECONDS = TIME.getSeconds() < 10 ? `0${TIME.getSeconds()}` : TIME.getSeconds();
+    const TIME_THE_CARD_WAS_CREATED = `${HOUR}:${MINUTE}:${SECONDS}`;
+    
+    return TIME_THE_CARD_WAS_CREATED;
+};
