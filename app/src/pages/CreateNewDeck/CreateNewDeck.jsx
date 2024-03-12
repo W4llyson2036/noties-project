6e714c7b479a96d1898c12fb2f35f8aa7c8824e2
W4llyson2036 <wallysonivo0111@gmail.com>
2024-03-12 14:32:48 -0300

    display message when creating new deck

--- app/src/pages/CreateNewDeck/CreateNewDeck.jsx
+++ app/src/pages/CreateNewDeck/CreateNewDeck.jsx
@@ -10,18 +10,29 @@ import { createDeck } from "../../firebase/createDeck";
 import './createNewDeck.css'
 
 export function CreateNewDeck() {
-    const [deckName, setDeckName] = useState('');
     const inputRef = useRef();
+    const [deckName, setDeckName] = useState('');
+    const [messageCreatedDeck, setMessageCreatedDeck] = useState({
+        state: null,
+        deckWasNotCreated: 'deck was not created', 
+        deckWasCreated: 'deck was created',
+    });
 
+    useEffect(() => {
+        inputFocus();
+    }, [deckName]);
+    
     function handleInput(value) {
         setDeckName(value);
     }
 
-    function inputFocus() { inputRef.current.focus()};
+    function inputFocus() { 
+        inputRef.current.focus()
+    }
 
-    useEffect(() => {
-        inputFocus();
-    }, []);
+    function cleanInput() { 
+        setDeckName(''); 
+    }
 
     return (
         <section className="section-create-new-deck">
@@ -34,13 +45,21 @@ export function CreateNewDeck() {
                     onChange={ev => handleInput(ev.target.value)}
                     />
 
+                <div className="display-message-when-creating-deck">
+                    {messageCreatedDeck.state === true && 
+                    <p className="deckWasCreated">{messageCreatedDeck.deckWasCreated}</p>}
+
+                    {messageCreatedDeck.state === false && 
+                    <p className="deckWasNotCreated">{messageCreatedDeck.deckWasNotCreated} </p>}
+                </div>
+
                 <UniversalButton 
                     value='create new deck' 
                     bg='#00A400'
                     width='50%'
                     margin='auto'
                     padding='1rem'
-                    click={() => createDeck(deckName)}
+                    click={() => createDeck(deckName, cleanInput, setMessageCreatedDeck)}
                     />
             </div>
         </section>
