e49943e33faf9a5d381b8248e07739674cfb46d7
W4llyson2036 <wallysonivo0111@gmail.com>
2024-09-10 08:13:46 -0300

    routes placed in a new file

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,34 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React              from 'react'
+import ReactDOM           from 'react-dom/client'
 
-// Components
-import { Login } from './pages/Login/Login'
-import { Home } from './pages/Home/Home/Home'
-import { CreateNewAccount } from './pages/CreateNewAccount/CreateNewAccount'
-import { MainLayout } from './layout/MainLayout'
-import { CreateNewDeck } from './pages/CreateNewDeck/CreateNewDeck'
-import { AddCard } from './pages/Home/AddCard/AddCard'
-import { ReviewCard } from './pages/Home/ReviewCard/ReviewCard'
-import { About } from './pages/About/About'
+// routes
+import { RouterProvider } from 'react-router-dom'
+import { routes }         from './Route/Route'
 
 // CSS
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-          <Route path='/' element={<Login />}/>
-          <Route path='/creatnewaccount' element={<CreateNewAccount />}/>
-          <Route element={<MainLayout />}>
-            <Route path='/home' element={<Home />}/>
-            <Route path='/home/createcard/:fordeckname/:id' element={<AddCard />}/>
-            <Route path='/home/review/:deckname/:id' element={<ReviewCard />}/>
-            <Route path='/createnewdeck' element={<CreateNewDeck />}/>
-            <Route path='/about' element={<About />}/>
-          </Route>  
-      </Routes>
-    </BrowserRouter>
+    <React.StrictMode>
+        <RouterProvider router={routes}> 
+    </RouterProvider>
   </React.StrictMode>
 )
---
+++ app/src/pages/ViewCards/ViewCards.jsx
@@ -0,0 +1,9 @@
+import './ViewCards.css';
+
+export function ViewCards() {
+    return (
+        <>
+            <h1 className="teste">ViewCards</h1>
+        </>
+    );
+}
